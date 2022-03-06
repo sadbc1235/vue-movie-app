75d40c643c9848000ab4f0d28f939fa98c573c36
sadbc1235 <sadbc1235@naver.com>
2022-03-06 23:38:46 +0900

    Create Project

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <Logo />
+    <a href="https://github.com/sadbc1235" target="_blank">
+      (C){{ new Date().getFullYear() }} KES
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo"
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    padding: 70px 0;
+    text-align: center;
+    opacity: .3;
+    .logo {
+      display: block;
+      margin-bottom: 4px;
+    }
+  }
+</style>
